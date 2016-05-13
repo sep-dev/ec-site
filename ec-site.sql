f0476e5e9b71be41e0568b89b3d3dc160e4be5ca
koyoariga <arigakoyo@se-project.co.jp>
2016-05-13 11:47:39 +0900

    間違えの変更

--- ec-site.sql
+++ ec-site.sql
@@ -4,11 +4,11 @@ create table tblClient(cilentId int primary key not null auto_increment,
 					cilentName nvarchar(255) not null,
 					clientKana nvarchar(255) not null,
 					clientSex nvarchar(255) not null,
-					clientBirthday date not null,
+					clientBirthday date,
 					clientPostCode varchar(8) not null,
 					clientAdd nvarchar(255) not null,
 					clientTel varchar(20) not null,
-					clientMailAddress nvarchar(255));
+					clientMailAddress nvarchar(255) not null);
 create table tblItem(itemId int primary key not null auto_increment,
 					itemName nvarchar(255) not null,
 					itemPrice int not null,
